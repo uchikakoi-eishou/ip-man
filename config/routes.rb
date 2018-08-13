ae1a35599fdba0cde7eccaf61e645516e2e2fc2a
uchikakoi-eishou <kakoi.0501souta@gmail.com>
2018-08-14 07:47:13 +0900

    change header p >> a

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'top' => 'home#top'
+  get '/' => 'home#top'
   get 'about' => 'home#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
