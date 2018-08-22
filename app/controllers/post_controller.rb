27f8ebe8263e252b4eca9fabf1a8512d18f5b27a
uchikakoi-eishou <kakoi.0501souta@gmail.com>
2018-08-22 13:01:05 +0900

    create common layouts

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -1,10 +1,5 @@
 class PostController < ApplicationController
   def index
-    @posts = [
-      "172.22.61.1",
-      "管理棟2階",
-      "デスクトップ",
-      "共用"
-    ]
+    @posts = Vlan.all
   end
 end
