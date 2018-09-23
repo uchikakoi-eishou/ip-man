1415177e7d90cce468cb54eb856ef312a34af8a2
uchikakoi-eishou <kakoi.0501souta@gmail.com>
2018-09-24 07:36:52 +0900

    create_edit.html.erb

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -24,8 +24,27 @@ class PostController < ApplicationController
                        user:           params[:user],
                        use:            params[:use])
     @post.save
+
     segment = params[:ip_address][8, 3]
     redirect_to("/post/index/#{segment}")
+
+  end
+
+  def edit
+    @post = Vlanid.find_by(id: params[:id])
+
+  end
+
+  def update
+    @post = Vlanid.find_by(id: params[:id])
+    @post.ip_address     = params[:ip_address]
+    @post.location       = params[:location]
+    @post.control_number = params[:control_number]
+    @post.user           = params[:user]
+    @post.use            = params[:use]
+    @post.save
+
+    redirect_to("/post/show/#{@post.id}")
 
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  get  "post/show/:id"       => "post#show"
-  get  "post/index/:segment" => "post#index"
-  get  "post/new"            => "post#new"
-  post "post/create"         => "post#create"
-  get  "/"                   => "home#top"
-  get  "about"               => "home#about"
+  get  "post/show/:id"        => "post#show"
+  get  "post/show/:id/edit"   => "post#edit"
+  post "post/show/:id/update" => "post#update"
+  get  "post/index/:segment"  => "post#index"
+  get  "post/new"             => "post#new"
+  post "post/create"          => "post#create"
+  get  "/"                    => "home#top"
+  get  "about"                => "home#about"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
